d5167845def66ac5fc8e291392cc77e4a081b0d9
lihe07 <li@imlihe.com>
2025-05-22 11:37:02 +0800

    changelog focus on latest

--- src/components/ChangeLog.vue
+++ src/components/ChangeLog.vue
@@ -4,7 +4,7 @@ import { NTimeline, NTimelineItem, NScrollbar } from 'naive-ui'
 
 <template>
   <n-timeline class="timeline">
-    <n-timeline-item type="" title="1.0.3" time="2025-05-22">
+    <n-timeline-item type="success" title="1.0.3" time="2025-05-22">
       修复桌面端编辑器界面元素溢出.
     </n-timeline-item>
     <n-timeline-item type="" title="1.0.2" time="2025-05-18">
@@ -13,7 +13,7 @@ import { NTimeline, NTimelineItem, NScrollbar } from 'naive-ui'
     <n-timeline-item type="" title="1.0.1" time="2024-03-23">
       加入分页和排序, 列表动态更新. 增加检测集群数量. 修改默认参数.
     </n-timeline-item>
-    <n-timeline-item type="success" title="1.0.0" time="2024-03-22">
+    <n-timeline-item type="" title="1.0.0" time="2024-03-22">
       修复多人同时编辑, 忙时编辑造成的冲突. 修复图片刷新缩放重置.
       <br />
       优化网络请求. 加入更新日志.
